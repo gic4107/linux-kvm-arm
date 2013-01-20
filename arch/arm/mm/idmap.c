b8c871dce6f98dd6547a17de75bd3dcd920dd113
Christoffer Dall <c.dall@virtualopensystems.com>
2013-01-20 18:43:10 -0500

    ARM: Section based HYP idmap
    Add a method (hyp_idmap_setup) to populate a hyp pgd with an
    identity mapping of the code contained in the .hyp.idmap.text
    section.
    
    Offer a method to drop this identity mapping through
    hyp_idmap_teardown.
    
    Make all the above depend on CONFIG_ARM_VIRT_EXT and CONFIG_ARM_LPAE.
    
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Reviewed-by: Marcelo Tosatti <mtosatti@redhat.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <c.dall@virtualopensystems.com>
    
    Conflicts:
    	arch/arm/include/asm/idmap.h
    	arch/arm/include/asm/pgtable-3level-hwdef.h
    	arch/arm/kernel/vmlinux.lds.S
    	arch/arm/mm/idmap.c

--- arch/arm/mm/idmap.c
+++ arch/arm/mm/idmap.c
@@ -1,8 +1,14 @@
+#include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/slab.h>
 
 #include <asm/cputype.h>
 #include <asm/pgalloc.h>
 #include <asm/pgtable.h>
+
+#include <asm/sections.h>
+#include <asm/system_info.h>
+#include <asm/virt.h>
 
 static void idmap_add_pmd(pud_t *pud, unsigned long addr, unsigned long end,
 	unsigned long prot)
@@ -28,11 +34,17 @@ static void idmap_add_pud(pgd_t *pgd, unsigned long addr, unsigned long end,
 	} while (pud++, addr = next, addr != end);
 }
 
-void identity_mapping_add(pgd_t *pgd, unsigned long addr, unsigned long end)
+static void identity_mapping_add(pgd_t *pgd, const char *text_start,
+				 const char *text_end, unsigned long prot)
 {
-	unsigned long prot, next;
+	unsigned long addr, end;
+	unsigned long next;
 
-	prot = PMD_TYPE_SECT | PMD_SECT_AP_WRITE;
+	addr = virt_to_phys(text_start);
+	end = virt_to_phys(text_end);
+
+	prot |= PMD_TYPE_SECT | PMD_SECT_AP_WRITE | PMD_SECT_AF;
+
 	if (cpu_architecture() <= CPU_ARCH_ARMv5TEJ && !cpu_is_xscale())
 		prot |= PMD_BIT4;
 
@@ -48,6 +60,46 @@ static void idmap_del_pmd(pud_t *pud, unsigned long addr, unsigned long end)
 {
 	pmd_t *pmd = pmd_offset(pud, addr);
 	pmd_clear(pmd);
+}
+
+#if defined(CONFIG_ARM_VIRT_EXT) && defined(CONFIG_ARM_LPAE)
+pgd_t *hyp_pgd;
+
+extern char  __hyp_idmap_text_start[], __hyp_idmap_text_end[];
+
+static int __init init_static_idmap_hyp(void)
+{
+	hyp_pgd = kzalloc(PTRS_PER_PGD * sizeof(pgd_t), GFP_KERNEL);
+	if (!hyp_pgd)
+		return -ENOMEM;
+
+	pr_info("Setting up static HYP identity map for 0x%p - 0x%p\n",
+		__hyp_idmap_text_start, __hyp_idmap_text_end);
+	identity_mapping_add(hyp_pgd, __hyp_idmap_text_start,
+			     __hyp_idmap_text_end, PMD_SECT_AP1);
+
+	return 0;
+}
+#else
+static int __init init_static_idmap_hyp(void)
+{
+	return 0;
+}
+#endif
+
+extern char  __idmap_text_start[], __idmap_text_end[];
+
+static int __init init_static_idmap(void)
+{
+	int ret;
+	pr_info("Setting up static identity map for 0x%p - 0x%p\n",
+		__idmap_text_start, __idmap_text_end);
+	identity_mapping_add(idmap_pgd, __idmap_text_start,
+			     __idmap_text_end, 0);
+
+	ret = init_static_idmap_hyp();
+
+	return ret;
 }
 
 static void idmap_del_pud(pgd_t *pgd, unsigned long addr, unsigned long end)
