31d793afed32c5c41200472fb9267884fa397654
Henrry <gic4107@hotmail.com>
2014-04-28 16:42:28 +0800

    Finish interrupt distributor with hash table implementation. Not test for multiple device distribution.

--- drivers/interrupt_distributor/interrupt_distributor.c
+++ drivers/interrupt_distributor/interrupt_distributor.c
@@ -6,58 +6,90 @@
 #include <linux/slab.h>
 #include <asm/uaccess.h>
 #include <linux/interrupt.h>
+#include <linux/list.h>
 
 #define DESC_NUM 10
+#define NAME_LEN 30
+
+#define hash_fn(dev_id) (unsigned int)((dev_id>>2)%DESC_NUM)
+
+struct list_head *irq_desc_hash;	// hash table array
 
 struct irq_desc_t {
 	unsigned int irq;
 	irq_handler_t handler;
 	unsigned long flags;
-	char *name;
+	char name[NAME_LEN];
 	void *dev;
-} *irq_desc;
+	struct list_head node;
+};
 int desc_count = 0;
 
 static irqreturn_t distributor(int irq, void* dev_id)
 {
-	printk("distributor function\n");
-	return irq_desc[0].handler(irq, dev_id);	
+	int hash_value = hash_fn((unsigned long)dev_id);
+	struct list_head *hash_head = &irq_desc_hash[hash_value];
+//	printk("distributor function ... ");
+//	printk("dev_id=0x%llx, hash value=%d\n", dev_id, hash_fn((unsigned long)dev_id));
+	struct irq_desc_t *irq_desc;
+	list_for_each_entry(irq_desc, hash_head, node) {
+		if(irq_desc->dev == dev_id)
+			return irq_desc->handler(irq, dev_id); 
+	}
 }
 
 int register_irq(unsigned int irq, irq_handler_t handler, unsigned long flags, const char *name, void *dev)
 {
-	printk("register_irq ... irq=%d, name=%s\n", irq, name);
-	printk("irq_desc=0x%llx\n", &irq_desc[0]);
-	printk("irq_desc.irq=0x%llx\n", &(irq_desc[0].irq));
-	printk("handler=0x%llx\n", (void*)handler);
-	printk("desc_count=0x%llx\n", &desc_count);
-	irq_desc[desc_count].irq = irq;
-printk("1\n");
-	irq_desc[desc_count].handler = handler;
-printk("2\n");
-	irq_desc[desc_count].flags = flags;
-printk("3\n");
-	irq_desc[desc_count].dev = dev;
-printk("4\n");
-	printk("irq_desc[0].name=0x%llx\n", &(irq_desc[0].name));
-	printk("name=0x%llx\n", name);	
-//	strcpy(irq_desc[desc_count++].name, name);
-printk("request_irq distributor=0x%llx\n", (void*)distributor);	
+	int hash_value = hash_fn((unsigned long)dev);
+	printk("register_irq ... irq=%d, name=%s, hash=%d", irq, name, dev, hash_fn((unsigned long)dev));
+	struct list_head *hash_head = &irq_desc_hash[hash_value];
+	struct irq_desc_t *irq_desc = kmalloc(sizeof(struct irq_desc_t), GFP_KERNEL);		
+	if(list_empty(hash_head)) {
+		printk("irq_desc_hash[%d] null\n", hash_value);
+		irq_desc->irq = irq;
+		irq_desc->handler = handler;
+		irq_desc->flags = flags;
+		irq_desc->dev = dev;
+		strcpy(irq_desc->name, name);	 
+		list_add(&(irq_desc->node), hash_head);	
+	}
+	else {
+		printk("irq_desc_hash[%d] not null\n", hash_value);	
+		while(1) {
+			struct list_head *iter = hash_head->next;
+			if(list_is_last(iter, hash_head)) {
+				irq_desc->irq = irq;
+				irq_desc->handler = handler;
+				irq_desc->flags = flags;
+				irq_desc->dev = dev;
+				strcpy(irq_desc->name, name);	 
+				list_add(&(irq_desc->node), hash_head);	
+			}
+		}
+
+	}
+	desc_count++;
 	return request_irq(irq, distributor, flags, name, dev);
 }
 EXPORT_SYMBOL(register_irq);
 
 static int __init distributor_init(void) {
 	printk("/****** Interrupt Distributor ******/\n");
-	irq_desc = kmalloc(DESC_NUM*sizeof(struct irq_desc_t), GFP_KERNEL);
-	if(!irq_desc)
-		printk("irq_desc kmalloc fail\n");
+	int i;
+	irq_desc_hash = kmalloc(DESC_NUM*sizeof(struct list_head), GFP_KERNEL);
+	if(!irq_desc_hash) {
+		printk("irq_desc_hash allocate fail\n");
+		goto out;
+	}	
+	for(i=0; i<DESC_NUM; i++)
+		INIT_LIST_HEAD(&(irq_desc_hash[i]));
+out:
 	return 0;
 }
 fs_initcall(distributor_init);
 
 static void __exit distributor_exit(void) {
-	printk("/****** BYE BYE ******/\n");
+	printk("/****** Exit INterrupt Distributor ******/\n");
 }
 module_exit(distributor_exit);
 
